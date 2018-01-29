9a72753a9e21116315194a7c73f01391256d7ffc
赵政 <www.a821400729@qq.com>
2018-01-29 16:58:54 +0800

    2018年1月29日 16点58分

--- dataPre/read_csv.py
+++ dataPre/read_csv.py
@@ -2,28 +2,72 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-data=pd.read_csv('52_csv.csv')
+# read csv
+def read_csv(filepath):
+    data=pd.read_csv(filepath)
+    return data 
 
-#print (data)
-#print (type(data))
+# 可视化show
+def show(data,filename='uname.jpg',colname='speed'):
+    plt.plot(data[colname])
+    plt.show()
+    #filename=filename+'.jpg'
+    #plt.savefig(filename)
 
-print (data['speed'])
-#plt.plot(data['speed'])
-#plt.show()
+# 计算某一列的数据缺失率
+def miss_rate(data,colname='speed'):
+    df = pd.isnull(data[colname])
+    df_list = df.tolist()
+    miss_rate = sum(df_list)/float(len(df_list))
+    print ("col : ",colname,", miss rate is : ",miss_rate)
 
-arr=np.array(data['speed'])
-speed_list = arr.tolist()
+# 合并数据
+def merge_data(data,colname='speed'):
+    arr=np.array(data['speed'])
+    speed_list = arr.tolist()
+    merge_step = 5
+    merge_length = len(speed_list) / merge_step 
+    merge_list=[]
+    for i in range(int(merge_length)):
+        begin_index=i*merge_step
+        merge_list.append(sum(speed_list[begin_index:begin_index+merge_step])/merge_step)
+    return merge_list
 
-#print (len(speed_list))
+# 简单的填充数据，将缺失的数据np.nan的数据 改成1
+def fillnan(data):
+    #以一个默认的数值 填充
+    #val=1.0
+    #filldata=data.fillna(val)
+    #前置值填充
+    #filldata=data.fillna(method='ffill')
+    #后置值填充
+    filldata=data.fillna(method='bfill')
+    return filldata
 
-#plt.plot(speed_list)
 
-merge_step = 5
-merge_length = len(speed_list) / merge_step 
-merge_list=[]
-for i in range(int(merge_length)):
-    begin_index=i*merge_step
-    merge_list.append(sum(speed_list[begin_index:begin_index+merge_step])/merge_step)
+#pandas 取一个时间段的数据
+def select_time_period(data):
+    #uperlimit='2012-11-15  5:57:57'
+    #lowerlimit='2012-11-15  5:55:57'
+    uperlimit = '2012-11-14  23:59:26'
+    lowerlimit = '2012-11-14  0:00:16'
+    data1 = data[data['last-update-time']< uperlimit]
+    data2 =data1[data1['last-update-time'] > lowerlimit]
+    print ("select data shape is : ",data2.shape)
+    return data2
 
-plt.plot(merge_list)
-plt.show()
+def generate_pic():
+    pass
+
+
+if __name__ =="__main__":
+    filepath='52_csv.csv'
+    data = read_csv(filepath)
+    #miss_rate(data)
+    #show(data)
+    filldata=fillnan(data)
+    print (filldata.head())
+
+    selectdata = select_time_period(data)
+    print (selectdata.head())
+    show(selectdata)
--- dataPre/test_csv.py
+++ dataPre/test_csv.py
@@ -2,6 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np 
 
+
+
+# 计算某一列的数据缺失率
+def miss_rate(data,colname='speed'):
+    df = pd.isnull(data[colname])
+    df_list = df.tolist()
+    miss_rate = sum(df_list)/float(len(df_list))
+    print (colname," miss rate is : ",miss_rate)
+
+
 data=pd.read_csv('test_csv.csv')
 
 #print (data)
@@ -14,3 +24,13 @@ arr = np.array(data['speed'])
 speed_list=arr.tolist()
 
 print (speed_list)
+
+
+print (miss_rate(data))
+
+"""
+data2 = pd.isnull(data['speed'])
+
+print ( data2[data2[0]==True] )
+#print ( pd.isnull(data['speed'])==True )
+"""
